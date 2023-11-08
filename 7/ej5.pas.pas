46110d1cc9a0cfa7ccdc694134ee0386b54274aa
Matias Guaymas <89048193+MatiasGuaymas@users.noreply.github.com>
2023-11-08 15:14:32 -0300

    CADP 2023

---
+++ 7/ej5.pas.pas
@@ -0,0 +1,258 @@
+{5. Una empresa de transporte de cargas dispone de la información de su flota compuesta por 100
+camiones. De cada camión se tiene: patente, año de fabricación y capacidad (peso máximo en
+toneladas que puede transportar).
+Realizar un programa que lea y almacene la información de los viajes realizados por la empresa. De
+cada viaje se lee: código de viaje, código del camión que lo realizó (1..100), distancia en kilómetros
+recorrida, ciudad de destino, año en que se realizó el viaje y DNI del chofer. La lectura finaliza cuando
+se lee el código de viaje -1.
+Una vez leída y almacenada la información, se pide:
+1. Informar la patente del camión que más kilómetros recorridos posee y la patente del camión que
+menos kilómetros recorridos posee.
+2. Informar la cantidad de viajes que se han realizado en camiones con capacidad mayor a 30,5
+toneladas y que posean una antigüedad mayor a 5 años al momento de realizar el viaje (año en
+que se realizó el viaje).
+3. Informar los códigos de los viajes realizados por choferes cuyo DNI tenga sólo dígitos impares.
+Nota: Los códigos de viaje no se repiten.}
+
+program ejercicio5_p7;
+
+const
+    cantCamiones = 10; {Deberían ser 100 por enunciado, pongo menos para probarlo}
+
+type
+    codigoCamion = 1..cantCamiones;
+    
+    camion = record         // Se dispone
+        patente: string;
+        anio: integer;
+        capacidad: real;
+    end;
+    
+    infoCamiones = array [codigoCamion] of camion; // Se dispone
+    
+    viaje = record
+        codeV: integer;
+        codeC: codigoCamion;
+        dist: real;
+        ciudad: string;
+        anio: integer;
+        dni: longint;
+    end;
+    
+    lista =^nodo;
+    nodo = record
+        dato: viaje;
+        sig: lista;
+    end;
+
+    vecSuma = array [codigoCamion] of real;
+
+{ZONA DE MODULOS}    
+procedure cargarUnCamion(var c: camion); // Se dispone
+begin
+    writeln;
+    write('Ingrese patente: ');
+    readln(c.patente);
+    write('Ingrese año: ');
+    readln(c.anio);
+    write('Ingrese capacidad: ');
+    readln(c.capacidad);
+    writeln;
+end;
+
+
+procedure cargarCamiones (var vC: infoCamiones); // Se dispone
+var
+    i: integer;
+    c: camion;
+
+begin
+    for i := 1 to cantCamiones do
+    begin
+        cargarUnCamion(c);
+        vC[i] := c;
+    end;
+end;
+
+
+procedure insertarOrdenado (var l: lista; v: viaje);
+var
+    nue: lista;
+    act, ant: lista;
+
+begin
+    new(nue);
+    nue^.dato := v;
+    
+    ant := l;
+    act := l; 
+    
+    if (l = nil) then
+        l := nue
+    else
+    begin
+
+        while (act <> nil) and (v.codeC > act^.dato.codeC) do 
+        begin
+            ant := act;
+            act := act^.sig;
+        end;
+    
+    
+        if (act = ant) then
+        begin
+            nue^.sig := l;
+            l := nue;
+        end
+        else
+        begin
+            ant^.sig := nue;
+            nue^.sig := act;
+        end
+    end;
+end;
+
+
+procedure cargarViaje (var v: viaje);
+begin
+    write('Ingrese codigo de viaje: ');
+    readln(v.codeV);
+    if (v.codeV <> -1) then
+    begin
+        write('Ingrese codigo de camion: ');
+        readln(v.codeC);
+        write('Ingrese distancia: ');
+        readln(v.dist);
+        write('Ingrese ciudad: ');
+        readln(v.ciudad);
+        write('Ingrese año: ');
+        readln(v.anio);
+        write('Ingrese dni: ');
+        readln(v.dni);
+    end;
+end;
+
+
+procedure cargarLista (var l: lista);
+var
+    v: viaje;
+    
+begin
+    cargarViaje(v);
+    writeln;
+    while (v.codeV <> -1) do
+    begin
+        insertarOrdenado(l, v);
+        cargarViaje(v);
+        writeln;
+    end;
+end;
+
+
+procedure imprimirViaje (v: viaje);
+begin
+    writeln;
+    writeln('Codigo de camion: ', v.codeC);
+    writeln('Codigo de viaje: ', v.codeV);
+    writeln('Distancia: ', v.dist:2:2);
+    writeln('Ciudad: ', v.ciudad);
+    writeln('Año: ', v.anio);
+    writeln('DNI: ', v.dni);
+    writeln;
+end;
+
+
+procedure imprimirLista (l: lista);
+begin
+    while (l <> nil) do
+    begin
+        imprimirViaje(l^.dato);
+        l := l^.sig;
+    end;
+end;
+
+
+{1. Informar la patente del camión que más kilómetros recorridos posee y la patente del camión que
+menos kilómetros recorridos posee.}
+procedure actualizarMax (var maxKm: real; var maxPat: string; kmAct: real; patAct: string);
+begin
+    if (kmAct > maxKm) then
+    begin
+        maxKm := kmAct;
+        maxPat := patAct;
+    end;
+end;
+
+
+procedure actualizarMin(var minKm: real; var minPat: string; kmAct: real; patAct: string);
+begin
+    if (kmAct < minKm) then
+    begin
+        minKm:=kmAct;
+        minPat:=patAct;
+    end;
+end;
+
+
+{2. Informar la cantidad de viajes que se han realizado en camiones con capacidad mayor a 30,5
+toneladas y que posean una antigüedad mayor a 5 años al momento de realizar el viaje (año en
+que se realizó el viaje).}
+function cumpleCondicionCapacidadyAntiguedad (c: camion; anioDelViaje: integer): boolean;
+begin
+    cumpleCondicionCapacidadyAntiguedad := (c.capacidad > 30.5) and (anioDelViaje - c.anio > 5);
+end;  
+
+
+{3. Informar los códigos de los viajes realizados por choferes cuyo DNI tenga sólo dígitos impares.
+Nota: Los códigos de viaje no se repiten.}
+procedure informaCodeVdeDNIimpar (dni: longint; codeV: integer);
+var 
+    dig: integer;
+    cumple : boolean;
+
+begin
+    cumple := true;
+    while (dni <> 0) do 
+    begin
+        dig := dni MOD 10;
+        if (dig MOD 2 <> 0) then
+            cumple := false;
+        dni := dni DIV 10;
+    end;
+    
+    if (cumple) then
+        writeln ('Codigo de viaje DNI impar: ', codeV);
+    writeln;
+end;
+
+
+{Recorrer lista y recabar información}
+procedure corteDeControl(l: lista; vecCamiones : infoCamiones);
+var 
+    camionActual: integer;
+    sumaKms: real;
+    patenteAct: string;
+
+begin
+    camionActual := l^.dato.codeC;
+    
+    while ((l <> nil) and (camionActual = l^.dato.codeC)) do
+    begin
+        
+    end;
+end;
+
+
+
+{PROGRAMA PRINCIPAL}
+var
+    l: lista;
+    vecCamiones: infoCamiones; // Se dispone
+
+begin
+    l := nil;
+    
+    cargarCamiones(vecCamiones); //Se dispone
+    cargarLista(l);
+    imprimirLista(l);
+end.
---
+++ 7/ej6.pas.pas
@@ -0,0 +1,246 @@
+{6. El Observatorio Astronómico de La Plata ha realizado un relevamiento sobre los distintos objetos
+astronómicos observados durante el año 2015. Los objetos se clasifican en 7 categorías: 1: estrellas,
+2: planetas, 3: satélites, 4: galaxias, 5: asteroides, 6: cometas y 7: nebulosas.
+Al observar un objeto, se registran los siguientes datos: código del objeto, categoría del objeto (1..7),
+nombre del objeto, distancia a la Tierra (medida en años luz), nombre del descubridor y año de su
+descubrimiento.
+A. Desarrolle un programa que lea y almacene la información de los objetos que han sido
+observados. Dicha información se lee hasta encontrar un objeto con código -1 (el cual no
+debe procesarse). La estructura generada debe mantener el orden en que fueron leídos los
+datos.
+B. Una vez leídos y almacenados todos los datos, se pide realizar un reporte con la siguiente
+información:
+1. Los códigos de los dos objetos más lejanos de la tierra que se hayan observado.
+2. La cantidad de planetas descubiertos por "Galileo Galilei" antes del año 1600.
+3. La cantidad de objetos observados por cada categoría.
+4. Los nombres de las estrellas cuyos códigos de objeto poseen más dígitos pares que
+impares}
+
+program PracticaRepaso_Ejercicio6;
+type
+
+    cat = 1..7;
+    str40 = string[40];
+    
+    cantXCate = array [cat] of integer;
+    
+    objeto = record
+        codigo: integer;
+        categoria: cat;
+        nombre: str40;
+        distancia: real;
+        nombreDesc: str40;
+        anio: integer;
+    end;
+    
+    lista=^nodo;
+    nodo = record
+        dato: objeto;
+        sig: lista;
+    end;
+
+
+
+procedure agregarAtras (var L, ult: lista; o: objeto);
+var
+    nue: lista;
+
+begin
+    new(nue);
+    nue^.dato := o;
+    
+    if (L = nil) then
+        L := nue
+    else
+        ult^.sig:=nue;
+    ult:=nue;
+end;
+
+
+procedure cargarObjeto (var o: objeto);
+begin
+    write('Ingrese codigo: ');
+    readln(o.codigo);
+    if (o.codigo <> -1) then
+    begin
+        write('Ingrese categoria: ');
+        readln(o.categoria);
+        write('Ingrese nombre: ');
+        readln(o.nombre);
+        write('Ingrese distancia: ');
+        readln(o.distancia);
+        write('Ingrese nombre del descubridor: ');
+        readln(o.nombreDesc);
+        write('Ingrese año de descubrimiento: ');
+        readln(o.anio);
+    end;
+    writeln;
+end;
+
+
+procedure cargarLista (var L: lista);
+var
+    o: objeto;
+    ult: lista;
+
+begin
+    cargarObjeto(o);
+    while (o.codigo <> -1) do
+    begin
+        agregarAtras(l, ult, o);
+        cargarObjeto(o);
+    end;
+end;
+
+
+procedure imprimirObjeto (o: objeto);
+begin
+    write('Codigo: ');
+    writeln(o.codigo);
+    write('Categoria: ');
+    writeln(o.categoria);
+    write('Nombre: ');
+    writeln(o.nombre);
+    write('Distancia: ');
+    writeln(o.distancia:2:2);
+    write('Nombre del descubridor: ');
+    writeln(o.nombreDesc);
+    write('Año de descubrimiento: ');
+    writeln(o.anio);
+    writeln;
+end;
+
+
+procedure imprimirLista (l: lista);
+begin
+    while (l <> nil) do
+    begin
+        imprimirObjeto(l^.dato);
+        l:= l^.sig;
+    end;
+end;
+
+
+procedure masLejanos (l: lista; var max1, max2: real; var cMax1, cMax2: integer);
+begin
+    if (l^.dato.distancia > max1) then
+    begin
+        max2:=max1;
+        cMax2:=cMax1;
+        max1:= l^.dato.distancia;
+        cMax1:= l^.dato.codigo;
+    end
+    else if (l^.dato.distancia > max2) then
+    begin
+        max2:= l^.dato.distancia;
+        cMax2:= l^.dato.codigo; 
+    end;
+end;
+
+
+procedure Galileo (descubridor: str40; var contador: integer);
+begin
+    if (descubridor = 'Galileo Galilei') then
+        contador := contador + 1;
+end;
+
+
+procedure inicializarContCatEn0 (var v: cantXCate);
+var
+    i: integer;
+
+begin
+    for i := 1 to 7 do
+        v[i]:= 0;
+end;
+
+
+procedure contarCategorias (c: integer; var v: cantXCate);
+begin
+    v[c] := v[c] + 1;
+end;
+
+
+procedure imprimirVector (v: cantXCate);
+var
+    i: integer;
+
+begin
+    for i := 1 to 7 do
+    begin
+        write(i, ': ');
+        writeln(v[i]);
+    end;
+end;
+
+
+{procedure masPqI (c: integer; var Lmpi: lista);
+var
+    contI, contP: integer;
+    aux: integer;
+    nue: lista;
+begin
+    contP, contI:=0;
+    
+    while (c <> 0) do 
+    begin
+        aux:= c mod 2;
+        if (aux = 0) then
+            contP:=contP+1;
+        else
+            contI:=contI + 1;
+        c := c div 10;
+    end;
+    
+    if (contP > contI) then
+        new(nue);
+        
+end;}
+
+
+procedure realizarInforme (l: lista);
+var
+    max1, max2: real;
+    cMax1, cMax2: integer;
+    contGG: integer;
+    contCate: cantXCate;
+
+begin
+    writeln('INFORMANDO...');
+    
+    max1:= -1;
+    contGG:= 0;
+    inicializarContCatEn0(contCate);
+    
+    while (l <> nil) do 
+    begin
+        masLejanos(l, max1, max2, cMax1, cMax2);
+        Galileo(l^.dato.nombreDesc, contGG);
+        contarCategorias(l^.dato.categoria, contCate);
+        l:= l^.sig;
+    end;
+    
+    writeln('Los objetos más lejanos son los de codigo ', cMax1, ' y ', cMax2);
+    writeln;
+    
+    writeln('Se ingresaron ', contGG, ' objetos descubiertos por Galileo Galilei');
+    writeln;
+    
+    writeln('La cantidad de objetos ingresados por categría, son: ');
+    imprimirVector(contCate);
+    writeln; 
+end;
+
+
+
+{ Programa principal }
+var
+    l: lista;
+
+begin
+    l:=nil;
+    cargarLista(l);
+    imprimirLista(l);
+    realizarInforme(l);
+    writeln('Falta resolver inciso D');
+end.
